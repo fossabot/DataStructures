f139169cab9feee7cb8e122a98c97c3c722d5a5f
Ximidar <ximidar@gmail.com>
2018-10-17 13:29:22 -0700

    Updated to deal with all golinter issues

--- replies.go
+++ replies.go
@@ -2,20 +2,17 @@
 * @Author: Ximidar
 * @Date:   2018-09-02 01:37:29
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-09-15 17:28:09
-*/
-package mango_structures
+* @Last Modified time: 2018-10-17 13:03:24
+ */
 
-import(
-	_"encoding/json"
-)
+package data_structures
 
-type Reply_String struct{
-	Success bool `json:"success"`
+type ReplyString struct {
+	Success bool   `json:"success"`
 	Message string `json:"message"`
 }
 
-type Reply_JSON struct{
-	Success bool `json:"success"`
+type ReplyJSON struct {
+	Success bool   `json:"success"`
 	Message []byte `json:"message"`
 }
--- tests/file_structure_tests/file_structures_test.go
+++ tests/file_structure_tests/file_structures_test.go
@@ -2,31 +2,32 @@
 * @Author: Ximidar
 * @Date:   2018-10-14 10:56:57
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-10-14 11:10:29
-*/
+* @Last Modified time: 2018-10-17 12:17:21
+ */
 
 package file_structure_test
 
-import(
+import (
 	"fmt"
 	"testing"
-	FS "github.com/ximidar/Flotilla/data_structures/file_structures"
+
+	FS "github.com/ximidar/Flotilla/data_structures/FileStructures"
 )
 
-func TestSetup(t *testing.T){
+func TestSetup(t *testing.T) {
 	fmt.Println("Testing Setup!")
 	test_path := "somepath"
-	fa, err := FS.New_File_Action(FS.SELECT_FILE, test_path)
+	fa, err := FS.NewFileAction(FS.SelectFile, test_path)
 
-	if err != nil{
+	if err != nil {
 		t.Fatal(err)
 	}
 
-	if fa.Action != FS.SELECT_FILE{
+	if fa.Action != FS.SelectFile {
 		t.Fatal("Action was not set correctly")
 	}
 
-	if fa.Path != test_path{
+	if fa.Path != test_path {
 		t.Fatal("Path was not set correctly")
 	}
 }
